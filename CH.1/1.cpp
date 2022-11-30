5d8b68879fedd950bed34f7dff4b9689804e1cf1
bara almustafa <73045713+bara11@users.noreply.github.com>
2022-11-30 18:16:36 +0300

    Create 1.cpp
    written with C language

---
+++ CH.1/1.cpp
@@ -0,0 +1,34 @@
+//1.Write a function that receives a string containing a 16-bit binary integer. The function must
+//return the string’s integer value
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string>
+
+int main() {
+	char sixteen_bit_number[16];
+	printf("Enter your binary\n");
+	scanf("%s", sixteen_bit_number);
+	int num;
+ num= atoi(sixteen_bit_number);
+	int binary_num= 0;
+	int reminder;
+	int decimal_num = 0;
+	 int base = 1;
+	 
+	 while (num >0)
+	 {
+		 reminder = num % 10;
+		 decimal_num = decimal_num + reminder * base;
+		 num = num / 10;
+		 base = base * 2;
+	}
+	 printf(" The binary number is %d \t", binary_num);
+	 printf(" \n The decimal number is %d \t", decimal_num);
+	 _getch();
+	 return 0;
+}
+
+
