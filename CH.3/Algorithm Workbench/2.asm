ffcbce05e1b655122e98b2c6ab97c3a4e2cd6cd6
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-02 01:35:25 +0300

    Create 2.asm

---
+++ CH.3/Algorithm Workbench/2.asm
@@ -0,0 +1,16 @@
+;2. Find out, by trial and error, if a program can have multiple code and data segments
+;ANSWER: NO ERROR
+; U CAN TRY BY UR SELF ;)
+
+.model flat
+.stack 100H
+.386
+.data
+var1 equ 25d
+var2 equ 11001b
+var3 equ 25o
+var4 equ 25h
+.data
+.code
+.data
+.data
