fb290e4e5b0fcba853f29840ce200adb110fa898
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-03 12:31:45 +0300

    Update 4.asm

--- CH.3/Algorithm Workbench/4.asm
+++ CH.3/Algorithm Workbench/4.asm
@@ -1,6 +1,6 @@
 ;4. Find out if you can declare a variable of type DWORD and assign it a negative
 ;value. What does this tell you about the assembler’s type checking?
-;try by ur self 
+;try by ur self ;)
 
 .model flat,stdcall
 .data
