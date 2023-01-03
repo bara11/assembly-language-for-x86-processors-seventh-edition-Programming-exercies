fec423ec5c1af89aaef5cfa7a26427d8f437d1dd
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-03 12:27:47 +0300

    Create 4.asm

---
+++ CH.3/Algorithm Workbench/4.asm
@@ -0,0 +1,13 @@
+;4. Find out if you can declare a variable of type DWORD and assign it a negative
+;value. What does this tell you about the assembler’s type checking?
+;try by ur self 
+
+.model flat,stdcall
+.data
+var1 dword -127;variable 1
+.code
+main:
+mov eax,var1
+
+ end main
+END
