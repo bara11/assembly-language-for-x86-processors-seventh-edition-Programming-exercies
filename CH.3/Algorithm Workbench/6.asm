df2c87352d5d1c05343b43a052cddb19cb193b1d
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-03 17:15:45 +0300

    Create 6.asm

---
+++ CH.3/Algorithm Workbench/6.asm
@@ -0,0 +1,17 @@
+!6. Given the number 456789ABh, list out its byte values in little-endian order.
+
+from internet i found the answer
+The given 32-bit value is 456789ab in hexadecimal notation.
+Each byte uses two hex digits. 
+The 4 bytes contained in the number are
+45 67 89 ab.
+
+Little-endian order stores the least significant bytes first in the smallest memory address.
+Therefore the storage in little-endian order is
+ab
+89
+67
+45
+
+Answer: 
+The byte values are ab, 89, 67, 45 !
