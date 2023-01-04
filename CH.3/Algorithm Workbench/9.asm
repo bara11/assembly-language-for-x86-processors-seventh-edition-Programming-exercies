b4be65aecbc7652368bfc310f27efc8283292eae
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-04 15:02:42 +0300

    Create 9.asm

---
+++ CH.3/Algorithm Workbench/9.asm
@@ -0,0 +1,13 @@
+;9. Declare a 32-bit signed integer variable and initialize it with the smallest possible
+;negative decimal value.
+
+
+.model flat,stdcall
+.stack 4096
+.data
+array1 SDWORD -128
+.code
+main:
+
+END main
+END
