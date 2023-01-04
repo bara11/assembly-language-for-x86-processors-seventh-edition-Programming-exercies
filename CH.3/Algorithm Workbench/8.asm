c019d9744381cae59da8eb146e1d0bda09254a60
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-04 14:59:25 +0300

    Create 8.asm

---
+++ CH.3/Algorithm Workbench/8.asm
@@ -0,0 +1,11 @@
+;8. Declare an array of byte and initialize it to the first 5 letters of the alphabet.
+
+.model flat,stdcall
+.stack 4096
+.data
+array1 BYTE 'A','B,'C,'D','E'
+.code
+main:
+
+END main
+END
