6345438500a4299163301add62a90a18ba5a24aa
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-05 19:00:57 +0300

    Create 10.asm

---
+++ CH.3/Algorithm Workbench/10.asm
@@ -0,0 +1,11 @@
+;10. Declare an unsigned 16-bit integer variable named wArray that uses three initializers
+
+.model flat,stdcall
+.stack 4096
+.data
+wArray BYTE 1,2,3
+.code
+main:
+
+END main
+END
