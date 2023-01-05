72ca022ab4f12e0c559718834724a29565d562b2
bara almustafa <73045713+bara11@users.noreply.github.com>
2023-01-05 19:03:36 +0300

    Create 11.asm

---
+++ CH.3/Algorithm Workbench/11.asm
@@ -0,0 +1,11 @@
+;11. Declare a string variable containing the name of your favorite color. Initialize it as
+;a nullterminated string
+.model flat,stdcall
+.stack 4096
+.data
+string1 dword "red",0
+.code
+main:
+
+END main
+END
